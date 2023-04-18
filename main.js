29009ae922efcfdd7549f43102cd6f969582864a
xllyllz <82016137+210213216219@users.noreply.github.com>
2023-04-18 19:07:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,136 +1,237 @@
-(async ()=> {
-let d = document;
-let head = d.head;
-let main_container = head.firstChild;
-let member_container = head.lastChild;
-let tmp_member = d.createElement("p");
-let tmp_cvs = d.createElement("canvas");
-let tmp_ctx = tmp_cvs.getContext("bitmaprenderer");
-let tmp_member_style;
-let active_member;
-let active_member_style;
-let ocvs = new OffscreenCanvas(240, 240);
-let octx = ocvs.getContext("2d");
-let buf = await createImageBitmap(await (await fetch(0)).blob());
-let name = "渡邉美穂0宮田愛萌0影山優佳0柿崎芽実0井口眞緒0松平璃子0関有美子0渡邉理佐0渡辺梨加0守屋茜0原田葵0菅井友香0米谷奈々未0平手友梨奈0長濱ねる0長沢菜々香0鈴本美愉0志田愛佳0佐藤詩織0織田奈那0尾関梨香0今泉佑唯0石森虹花0大園桃子0渡辺みり愛0米徳京花0山崎怜奈0矢田里沙子0堀未央奈0西川七海0寺田蘭世0鈴木絢音0新内眞衣0佐々木琴子0相楽伊織0北野日奈子0伊藤純奈0伊藤かりん0和田まあや0若月佑美0大和里菜0宮澤成良0松村沙友理0星野みなみ0深川麻衣0樋口日奈0橋本奈々未0畠中清羅0能條愛未0西野七瀬0永島聖羅0中元日芽香0中田花奈0高山一実0白石麻衣0桜井玲香0斉藤優里0斎藤ちはる0齋藤飛鳥0川村真洋0川後陽菜0柏幸奈0衛藤美彩0岩瀬佑美子0井上小百合0伊藤万理華0伊藤寧々0市來玲奈0生駒里奈0生田絵梨花0安藤美雲0秋元真夏0渡辺莉奈0山下葉留花0宮地すみれ0藤嶌果歩0平岡海月0平尾帆夏0竹内希来里0正源司陽子0清水理央0小西夏菜実0岸帆夏0石塚瑶季0山口陽世0森本茉莉0髙橋未来虹0上村ひなの0松田好花0濱岸ひより0丹生明里0富田鈴花0小坂菜緒0河田陽菜0金村美玖0東村芽依0高本彩花0高瀬愛奈0佐々木美玲0佐々木久美0齊藤京子0加藤史帆0潮紗理菜0山下瞳月0村山美羽0村井優0向井純葉0的野美青0中嶋優月0谷口愛李0小島凪紗0小田倉麗奈0遠藤理子0石森璃花0山﨑天0守屋麗奈0森田ひかる0松田里奈0増本綺良0藤吉夏鈴0田村保乃0武元唯衣0幸阪茉里乃0大沼晶保0大園玲0遠藤光莉0井上梨名0土生瑞穂0齋藤冬優花0小林由依0小池美波0上村莉菜0中西アルノ0冨里奈央0菅原咲月0川﨑桜0奥田いろは0小川彩0岡本姫奈0井上和0一ノ瀬美空0池田瑛紗0五百城茉央0弓木奈於0矢久保美緒0松尾美佑0林瑠奈0早川聖来0筒井あやめ0田村真佑0清宮レイ0柴田柚菜0佐藤璃果0黒見明香0北川悠理0金川紗耶0掛橋沙耶香0賀喜遥香0遠藤さくら0与田祐希0吉田綾乃0山下美月0向井葉月0中村麗乃0佐藤楓0阪口珠美0久保史緒里0梅沢美波0岩本蓮加0伊藤理々杏".split(0);
-let palette = ["b32","c83","b91","692","193"];
-let i_0 = 169;
-let i_1 = 0;
-let i_2 = 3840;
-let i_3 = 0;
-let elm = d.createElement("a");
-let timer;
+(async d=> {
+let head = d.head,
+    member_container = head.firstChild,
+    theme_container = head.lastChild,
+    main_container = d.createElement("P"), //head.lastChild,
+    member_box = d.createElement("P"),
+    member_cvs = d.createElement("canvas"),
+    member_ctx = member_box.appendChild(member_cvs).getContext("bitmaprenderer"),
+    member_ocvs = new OffscreenCanvas(member_cvs.width = 240, member_cvs.height = 240),
+    member_octx = member_ocvs.getContext("2d"),
+    member_src = await createImageBitmap(await(await fetch(0)).blob()),
+    member_name = [
+      "伊藤理々杏","岩本蓮加","梅沢美波","久保史緒里","阪口珠美",
+      "佐藤楓","中村麗乃","向井葉月","山下美月","吉田綾乃",
+      "与田祐希","遠藤さくら","賀喜遥香","掛橋沙耶香","金川紗耶",
+      "北川悠理","黒見明香","佐藤璃果","柴田柚菜","清宮レイ",
+      "田村真佑","筒井あやめ","早川聖来","林瑠奈","松尾美佑",
+      "矢久保美緒","弓木奈於","五百城茉央","池田瑛紗","一ノ瀬美空",
+      "井上和","岡本姫奈","小川彩","奥田いろは","川﨑桜",
+      "菅原咲月","冨里奈央","中西アルノ","上村莉菜","小池美波",
+      "小林由依","齋藤冬優花","土生瑞穂","井上梨名","遠藤光莉",
+      "大園玲","大沼晶保","幸阪茉里乃","武元唯衣","田村保乃",
+      "藤吉夏鈴","増本綺良","松田里奈","森田ひかる","守屋麗奈",
+      "山﨑天","石森璃花","遠藤理子","小田倉麗奈","小島凪紗",
+      "谷口愛李","中嶋優月","的野美青","向井純葉","村井優",
+      "村山美羽","山下瞳月","潮紗理菜","加藤史帆","齊藤京子",
+      "佐々木久美","佐々木美玲","高瀬愛奈","高本彩花","東村芽依",
+      "金村美玖","河田陽菜","小坂菜緒","富田鈴花","丹生明里",
+      "濱岸ひより","松田好花","上村ひなの","髙橋未来虹","森本茉莉",
+      "山口陽世","石塚瑶季","岸帆夏","小西夏菜実","清水理央",
+      "正源司陽子","竹内希来里","平尾帆夏","平岡海月","藤嶌果歩",
+      "宮地すみれ","山下葉留花","渡辺莉奈","秋元真夏","安藤美雲",
+      "生田絵梨花","生駒里奈","市來玲奈","伊藤寧々","伊藤万理華",
+      "井上小百合","岩瀬佑美子","衛藤美彩","柏幸奈","川後陽菜",
+      "川村真洋","齋藤飛鳥","斎藤ちはる","斉藤優里","桜井玲香",
+      "白石麻衣","高山一実","中田花奈","中元日芽香","永島聖羅",
+      "西野七瀬","能條愛未","畠中清羅","橋本奈々未","樋口日奈",
+      "深川麻衣","星野みなみ","松村沙友理","宮澤成良","大和里菜",
+      "若月佑美","和田まあや","伊藤かりん","伊藤純奈","北野日奈子",
+      "相楽伊織","佐々木琴子","新内眞衣","鈴木絢音","寺田蘭世",
+      "西川七海","堀未央奈","矢田里沙子","山崎怜奈","米徳京花",
+      "渡辺みり愛","大園桃子","石森虹花","今泉佑唯","尾関梨香",
+      "織田奈那","佐藤詩織","志田愛佳","鈴本美愉","長沢菜々香",
+      "長濱ねる","平手友梨奈","米谷奈々未","菅井友香","原田葵",
+      "守屋茜","渡辺梨加","渡邉理佐","関有美子","松平璃子",
+      "井口眞緒","柿崎芽実","影山優佳","宮田愛萌","渡邉美穂"
+    ],
+    palette = ["b32","c83","b91","692","193"],
+    elm_0 = d.createElement("a"), // 
+    elm_1, //d.createElement("i"),
+    i_0 = 0,
+    i_1 = 7,
+    i_2 = -3840;
+    i_3 = 0;
+
 
 head.setAttribute("style",
-    "display:block;width:min(100%,1028px);margin:auto;background:#123;font:10px meiryo;text-align:center;color:#ddd;overflow:hidden;touch-action:none;-webkit-user-select:none;-webkit-touch-callout:none");
-main_container.setAttribute("style", "display:block;height:calc(100% - 96px);padding:8 0 240;overflow:auto");
+  "display:block;\
+  background:#123;\
+  font:10px meiryo;\
+  text-align:center;\
+  color:#eee;\
+  overflow:hidden;\
+  -webkit-user-select:none;\
+  -webkit-touch-callout:none");
+
+// max(8px, calc((100% - 1024px) / 2))
+main_container.setAttribute("style", 
+  "width:100%;\
+  height:calc(100% - 96px);\
+  padding:8 max(8px, 1%);\
+  box-sizing: border-box;\
+  overflow-y:scroll");
+
+theme_container.setAttribute("style",
+  "display:flex");
+
 member_container.setAttribute("style",
-  "display:flex;position:fixed;left:0;bottom:0;min-width:100%;width:100%;height:80;padding:4;background:#345;overflow:auto");
+  "display:flex;position:fixed;bottom:0;\
+  width:100%;height:80;padding:4 0;\
+  background:#345;overflow-x:scroll");
 
-elm.href = "https://twitter.com/ariamaranai";
-elm.setAttribute("style", "display:contents;color:#399");
-elm.append("@ariamaranai");
-main_container.append(d.title = "tiermaker46 ", elm);
+elm_0.href = "https://twitter.com/ariamaranai";
+elm_0.append("@ariamaranai");
 
-tmp_cvs.height = tmp_cvs.width = 240;
-tmp_member.append(tmp_cvs);
+// 右端にある必要性がない flexにしてscrollするだけで色を選べるようにする
 
-while (
-  i_0 < [170,132,103,72,21,12,5,0][i_1] &&
-    (tmp_member.setAttribute("style", "color:#" + ["ecf","d9c","8ce","c9d","9db","b89","9df"][i_1]), ++i_1),
-  octx.drawImage(buf, i_2, i_3, 240, 240, 0, 0, 240, 240),
-  member_container.appendChild(elm = tmp_member.cloneNode(1)).lastChild.getContext("bitmaprenderer").transferFromImageBitmap(ocvs.transferToImageBitmap()),
-  elm.append(name[i_0]),
-  i_0
-) --i_0, i_2 = i_2 ? i_2 - 240 : (i_3 += 240, 3840);
+head.appendChild(main_container).append(d.title = "tiermaker46 ", elm_0);
+member_octx.arc(120, 120, 120, 0, 7);
+member_octx.clip();
 
 while (
-  main_container.appendChild(elm = d.createElement("b")).append("SABCD"[i_0]),
-  elm.setAttribute("style", "background:#"+ palette[i_0]),
-  main_container.appendChild(elm = d.createElement("i")),
-  ++i_0 < 5
-);
+  [170,165,158,149,98,67,38,0][i_1] <= i_0 &&
+  member_box.setAttribute("style",
+    "margin:0;padding:2;color:#" +
+    ["9df","b89","9db","c9d","8ce","d9c","ecf"][--i_1]),
+  member_octx.drawImage(member_src, i_2, i_3),
+  elm_1 = member_container.appendChild(elm_0 = member_box.cloneNode(1)).lastChild,
+  elm_1.getContext("bitmaprenderer").transferFromImageBitmap(member_ocvs.transferToImageBitmap()),
+  elm_1.append(String.fromCharCode(i_0)),
+  elm_0.append(member_name[i_0]),
+  ++i_0 < 170
+) i_2 = i_2 ? i_2 + 240 : (i_3 -= 240, -3840);
 
+while (
+  main_container.appendChild(elm_0 = d.createElement("I")).append("SABCD "[i_1]),
+  elm_0.setAttribute("contenteditable", "plaintext-only"),
+  main_container.appendChild(d.createElement("B")),
+  //elm_0.setAttribute("style", "background:#"+ ["b32","c83","b91","692","193","800"][i_1]),
+  i_1 < 5
+) ++i_1;
 
-member_container.onwheel =E=> {
-  member_container.scrollBy(E.deltaY < 0 ? -64 : 64, 0);
+head.appendChild(member_cvs).setAttribute("style", "position:fixed;left:-32;top:-32");
+
+let grab_member =()=> {
+  onpointermove =E=> {
+    elm_0.attributeStyleMap.set("filter", new CSSUnparsedValue(["brightness(.6)"]));
+    member_octx.drawImage(elm_0.firstChild, 0, 0);
+    member_ctx.transferFromImageBitmap(member_ocvs.transferToImageBitmap());
+
+    (onpointermove =E=> {
+      member_cvs.attributeStyleMap.set("transform", new CSSTransformValue([new CSSTranslate(CSS.px(E.x), CSS.px(E.y))]))
+    })(E);
+
+    onpointerover =E=> {
+      elm_1 = E.target;
+      let tagName = elm_1.tagName;
+      if (tagName == "B") {
+        elm_1.appendChild(elm_0);
+      } else if (tagName == "P" && E.layerY > 84) {
+        elm_1.before(elm_0);
+      } else if (E.layerY < 84 && elm_0.parentNode.tagName == "B") {
+        let txt = member_container.textContent;
+        let c_0 = elm_0.textContent.charCodeAt(0), c_1;
+        let i = 0 , j = 0;
+        while (i < txt.length && ((c_1 = txt.charCodeAt(i)) > 170 || c_0 < c_1 || ++j)) ++i;
+        member_container.insertBefore(elm_0, member_container.children[j])
+          .scrollIntoView({inline: "center"});
+      }
+    }
+  
+    onpointerup =()=> {
+      onpointermove = null;
+      onpointerover = null;
+      onpointerenter = null;
+      onpointerleave = null;
+      onpointerup = null;
+      member_ctx.transferFromImageBitmap(member_ocvs.transferToImageBitmap());
+      elm_0.attributeStyleMap.delete("filter");
+      elm_0 = null;
+      elm_1 = null;
+    }
+
+  }
 }
 
-tmp_member_style = tmp_member.attributeStyleMap;
-tmp_member_style.set("display", "none");
-tmp_member_style.set("position", "fixed");
-head.appendChild(tmp_member).append(new Text);
+let grab_label =E=> {
+  onpointermove =E=> {
+    let elm_1 = elm_0.nextSibling;
+    let bgcolor = elm_0.getAttribute("style");
+    elm_0.setAttribute("style", "position:fixed;top:-9px;filter:brightness(.6);" + bgcolor);
+    elm_1.setAttribute("style", "position:fixed;top:0px;filter:brightness(.6)");
+    let y = E.y;
 
-oncontextmenu =()=> !1;
+    (onpointermove =E=> {
+      elm_0.attributeStyleMap.set("transform", new CSSTransformValue([new CSSTranslate(CSS.px(0), CSS.px(E.y))]))
+      elm_0.attributeStyleMap.set("transform", new CSSTransformValue([new CSSTranslate(CSS.px(0), CSS.px(E.y))]))
+    })(E);
 
-let ff =()=> {
-  onpointermove = null;
-  onscroll = null
-  onwheel = null;
-  onpointerup = null;
-  onpointerleave = null;
+
+
+    onpointerup =()=> {
+      onpointermove = null;
+      onpointerover = null;
+      onpointerenter = null;
+      onpointerleave = null;
+      onpointerup = null;
+      elm_0.setAttribute("style", bgcolor);
+      elm_1.removeAttribute("style");
+      elm_0 = null;
+      elm_1 = null;
+    }
+  }
 }
 
 onpointerdown =E=> {
   if (E.button) return;
-  elm = E.target;
-  let tagName = elm.tagName;
+  elm_0 = E.target;
+  let tagName = elm_0.tagName;
   if (tagName == "P") {
-    pt_x = E.x;
-    pt_y = E.y;
-    onpointermove =()=> (clearTimeout(timer), ff());
-    onscroll =()=> (clearTimeout(timer), ff());
-    onwheel =()=> (clearTimeout(timer), ff());
-    onpointerup =()=> (clearTimeout(timer), ff());
-    onpointerleave =()=> (clearTimeout(timer), ff());
-
-    timer = setTimeout(()=> {
-      ff();
-      onpointerleave = null;
-      elm = elm.firstChild;
-      octx.drawImage(elm, 0, 0);
-      tmp_ctx.transferFromImageBitmap(ocvs.transferToImageBitmap());
-      tmp_member.lastChild.data = elm.nextSibling.data;
-      active_member = elm.parentNode;
-      active_member_style = active_member.attributeStyleMap;
-      active_member_style.set("filter", new CSSUnparsedValue(["brightness(.5)"]));
-      tmp_member_style.set("color",active_member_style.get("color"));
-      
-      onpointerup =()=> {
-        onpointermove = null;
-        onpointerover = null;
-        onpointerenter = null;
-        if (active_member) {
-          tmp_member_style.set("display", "none");
-          active_member_style.delete("filter");
-          active_member = null;
-        }
+    if (E.pointerType != "mouse") {
+      member_container.onscroll =E=> {
+        E.preventDefault();
       }
-      
-      (onpointermove =E=> {
-        tmp_member_style.set("left", CSS.px(E.x - 32));
-        tmp_member_style.set("top", CSS.px(E.y - 40));
-      })(E);
-
-      onpointerover =E=> {
-        elm = E.target;
-        let tagName = elm.tagName;
-        if (tagName == "I") {
-          elm.append(active_member);
-        } else if (tagName == "P" && elm.parentNode.tagName == "I") {
-          elm.before(active_member);
-        } else if (tagName == "SCRIPT" && active_member.parentNode.tagName == "I") {
-        }
-      }
-
       main_container.onscroll =E=> {
         E.preventDefault();
       }
-
-      tmp_member_style.set("display", "block");
-      tmp_member_style.set("pointer-events", "none");
-    }, 240);
+    } else {
+      grab_member();
+    }
+  } else if (tagName == "I") {
+    //grab_label(E);
   }
 }
-d.lastChild.replaceWith(head);
-})()
+
+onclick =E=> {
+  elm_0 = E.target;
+}
+
+member_container.onwheel =E=> {
+  member_container.scrollBy(E.deltaY < 0 ? -64 : 64, 0);
+}
+/*
+let grab2 =E=> {
+  let node = selected_elm_0.nextSibling;
+  node.setAttribute("style", "position:fixed;top:0;filter:brightness(.6)")
+  let y = E.y;
+  (onpointermove =E=> {
+    selected_elm_0_style.set("transform", new CSSTransformValue([new CSSTranslate(CSS.px(0), CSS.px(E.y - 9))]));
+    node.attributeStyleMap.set("transform", new CSSTransformValue([new CSSTranslate(CSS.px(0), CSS.px(E.y + 13))]));
+  })(E);
+  onpointerover =E=> {
+    elm_0 = E.target;
+    tagmember_name = elm_0.tagmember_name;
+    if (tagmember_name == "B") {
+      if (E.y < y) {
+        elm_0.previousSibling.before(selected_elm_0, selected_elm_0.nextSibling);
+      } else {
+        elm_0.after(selected_elm_0, selected_elm_0.nextSibling);
+      }
+    }
+  }
+}
+*/
+//console.log(d.lastChild.lastChild.lastChild);
+d.replaceChild(head, d.lastChild); // DOMContentLoadedされてからの値が定義できる replaceChildは
+})(document)
