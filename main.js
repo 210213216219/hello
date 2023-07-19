ad85cfc91f9434d85d0ff1afa9058d0c20dd44eb
xllyllz <82016137+210213216219@users.noreply.github.com>
2023-07-19 09:30:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,143 +1,129 @@
 ondragstart =()=> !1;
 
 {
-onpointerdown =a=> {
-  if (a.button) return;
-  let tagName = (target_0 = a.target).tagName;
-  
-  if (tagName == "P") {
-    if (target_0.textContent == "◢") {
-    } else {
-    point_y = a.y;
-    target_0.attributeStyleMap.set("filter", filter);
-    target_not_tier = member_container.contains(target_0);
-    member_container.ontouchmove =a=> a.preventDefault();
-
-    onpointermove = target_not_tier ?
-      a=> {
-        if (a.y - point_y < -8) {
-          onpointerover = insertMember;
-          octx.drawImage(target_0.firstChild, 0, 0);
-          bmp.transferFromImageBitmap(ocvs.transferToImageBitmap());
-          (onpointermove = moveMember)(a);
-        } else {
-
-        }
-      } :
-      (
-        octx.drawImage(target_0.firstChild, 0, 0),
-        bmp.transferFromImageBitmap(ocvs.transferToImageBitmap()),
-        moveMember(a),
-        onpointerover = insertMember,
-        moveMember
-      )
-
-    onpointerup =a=> {
-      if (!(a.y ^ point_y)) {
-
-      }
-
-      target_1 ?
-        (target_1.attributeStyleMap.clear(),
-         target_not_tier && target_0.attributeStyleMap.set("display", none)) :
-        target_0.attributeStyleMap.clear();
-      
-      octx.clearRect(0, 0, 240, 240);
-      bmp.transferFromImageBitmap(ocvs.transferToImageBitmap());
-
-      onpointermove =
-      onpointerover =
-      onpointerup =
-      member_container.ontouchmove =
-      target_0 =
-      target_1 = null;
-
-    }
-
-    }
-  } else if (tagName == "H4") {
-    /*
-    point_y = a.y;
-    let styleMap = target_0.attributeStyleMap;
-    styleMap.set("filter", filter);
-    onscroll =a=> a.preventDefault();
-    (onpointermove =a=> {
-      let y = point_y - a.y;
-      if (y > 127) {
-        target_1 = target_0.previousElementSibling;
-        target_1.tagName == "H4" && target_1.before(target_0);
-        point_y = a.y;
-      } else if (y < -127) {
-        target_1 = target_0.nextElementSibling;
-        target_1.tagName == "H4" && target_1.after(target_0);
-        point_y = a.y;
-      }
-    })(a);
-    onpointerup =a=> {
-      styleMap.clear();
-      onpointermove =
-      onpointerup =
-      onscroll = null;
-
-
-      if (!(a.y ^ point_y)) {
-        let input = target_0.firstChild;
-        input.disabled = !1;
-        input.focus();
-        input.onblur =()=> (input.disabled = !0);
-      }
-
-      target_0 =
-      target_1 = null;
-    }*/
-  } else if (tagName == "B") {
-    
-  }
-}
-
 let d = document;
-let member_container = d.body.lastElementChild;
+let body = d.body;
+let tierContainer = body.firstChild.lastChild;
+let memberContainer = body.lastChild;
+let addContainer = document.getElementsByTagName("h4")[5];
 let cvs = d.createElement("canvas");
-let bmp = cvs.getContext("bitmaprenderer");
-let ocvs = new OffscreenCanvas(cvs.width = 240, cvs.height = 240);
-let octx = ocvs.getContext("2d");
+let ctx = cvs.getContext("2d");
+let fBrightness = new CSSUnparsedValue(["brightness(.6)"]);
+let finvert = new CSSUnparsedValue(["invert(.4)"])
 let transform = new CSSTransformValue([new CSSTranslate(CSS.px(0), CSS.px(0))]);
-let filter = new CSSUnparsedValue(["brightness(.6)"]);
 let none = new CSSKeywordValue("none");
-let moveMember =a=> cvs.attributeStyleMap.set("transform", (transform[0].x.value = a.x, transform[0].y.value = a.y, transform));
-let insertMember =a=> {
-  alert(1);
-  a = a.target;
-  if (a.tagName == "I") {
-    if (a == member_container) {
-      if (member_container.contains(target_0)) {
-      } else {
-        if (!target_not_tier) {
-          let children = member_container.children, i = children.length;
-          while (children[--i].textContent != target_0.textContent && i);
-          children[i].attributeStyleMap.delete("display");
-          target_0.remove();
-          (target_1 = children[i]).scrollIntoView({inline: "center"});
-        } else {
-        }
-      }
-    } else {
-      a.appendChild(target_1 ??= target_not_tier ? target_0.cloneNode(1) : target_0);
-    }
-  } else if (a.tagName == "P" && !member_container.contains(a)) {
-    a.before(target_1 ??= target_not_tier ? target_0.cloneNode(1) : target_0);
-  }
-}
-
+let mcY = memberContainer.getBoundingClientRect().y - 8;
+let grabstart =a=> {
+  target_0.attributeStyleMap.set("filter", fBrightness);
+  ctx.drawImage(target_0.firstChild, 0, 0);
+  cvs.setPointerCapture(a.pointerId);
+  cvs.attributeStyleMap.set("transform", (transform[0].x.value = a.x, transform[0].y.value = a.y, transform));
+  cvs.attributeStyleMap.delete("display");
+};
 let target_0;
 let target_1;
-let target_not_tier;
-let point_y;
+let target_2;
+let target_in_tier;
+let pointY;
 
+memberContainer.addEventListener("wheel", a=> memberContainer.scrollBy(a.deltaY < 0 ? -64 : 64, 0), {passive: !0});
 
-member_container.addEventListener("wheel", a=> member_container.scrollBy(a.deltaY < 0 ? -64 : 64, 0), {passive: !0});
+onpointerdown =a=> {
+  if (a.button) return;
+  target_0 = a.target;
+  let tagName = target_0.tagName;
+  if (tagName == "P") {
+    target_in_tier = !memberContainer.contains(target_0);
+    pointY = a.y;
 
-octx.arc(120, 120, 120, 0, 7);
-octx.clip();
-d.body.appendChild(cvs);
+    let type = a.pointerType;
+    if (target_in_tier) {
+      if (type == "mouse") {
+        grabstart(a);
+      } else {
+        memberContainer.ontouchmove =a=> a.preventDefault();
+      }
+    } else {
+      if (type == "mouse") {
+        grabstart(a);
+      } else {
+        memberContainer.ontouchmove =a=> {
+          if (a.y - pointY < -16) {
+            memberContainer.ontouchmove =
+            tierContainer.ontouchmove=a=> a.preventDefault();
+            grabstart(a);
+          }
+        }
+      }
+    }
+  } else if (tagName == "H4") {
+
+  } else if (tagName == "S") {
+
+  }
+}
+
+cvs.onpointermove =a=> {
+  let x = a.x, y = a.y;
+  cvs.attributeStyleMap.set("transform", (transform[0].x.value = x, transform[0].y.value = y, transform))
+  if (y < mcY) {
+    let e = d.elementFromPoint(x, y);
+    if (e != target_2) {
+      target_2 = e;
+      //if (e != memberContainer) {
+        let tagName = e.tagName;
+        if (tagName == "I") {
+          if (addContainer.contains(e)) {
+            addContainer.attributeStyleMap.set("filter", finvert);
+          } else {
+            !e.contains(target_1 ??= target_in_tier ? target_0 : target_0.cloneNode(1)) &&
+            (e.appendChild(target_1));
+          }
+        } else if (tagName == "P") {
+          //e.before(target_1 ??= target_in_tier ? target_0 : target_0.cloneNode(1));
+          target_1 ??= target_in_tier ? target_0 : target_0.cloneNode(1);
+          let next = e.nextSibling;
+          e.parentNode.insertBefore(target_1, next == target_1 ? e : next);
+        }
+      //}
+    }
+  } else {
+    if (target_in_tier) {
+      let children = memberContainer.children, i = children.length;
+      while (children[--i].textContent != target_0.textContent && i);
+      children[i].attributeStyleMap.delete("display");
+      (target_1 = children[i]).scrollIntoView({inline: "center"});
+      target_0.remove();
+    }
+  }
+};
+
+cvs.onpointerup =a=> {
+  cvs.attributeStyleMap.set("display", none);
+  cvs.releasePointerCapture(a.pointerId);
+
+  target_1 ? (target_1.attributeStyleMap.clear(),
+   target_in_tier || target_0.attributeStyleMap.set("display", none)) :
+  target_0.attributeStyleMap.clear();
+
+  addContainer.attributeStyleMap.clear();
+  let y = addContainer.getBoundingClientRect().y;
+  if (y < a.y && y + 99 > a.y) {
+    (a = addContainer.cloneNode(1)).firstChild.attributeStyleMap.clear();
+    tierContainer.insertBefore(a, addContainer).lastChild.appendChild(target_0);
+  }
+
+  memberContainer.ontouchmove =
+  target_0 =
+  target_1 =
+  target_2 =
+  null;
+}
+
+cvs.attributeStyleMap.set("transform", transform);
+cvs.attributeStyleMap.set("display", none);
+cvs.width = cvs.height = 240;
+ctx.arc(120, 120, 120, 0, 7);
+ctx.clip();
+body.appendChild(cvs);
 }
